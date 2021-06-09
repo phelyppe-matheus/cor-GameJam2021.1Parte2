6b8b11362d40d6c8b6973b96dac27cd128300e4a
PhePheMath <PhelyppeMath@outlook.com>
2021-06-08 22:54:04 -0300

    O menino cresceu, já tá andando

--- src/characters/Actors.gd
+++ src/characters/Actors.gd
@@ -1,17 +1,16 @@
 extends KinematicBody2D
 class_name Actor
 
-export var gravity := 300;
-export var speed := Vector2(300, 1000);
-export var jump := Vector2(0, 200);
+export var gravity := 500;
+export var speed := Vector2(400, 500);
 
+var FLOOR_NORMAL = Vector2.UP
 var _velocity = Vector2.ZERO;
 #func _ready():
 #	pass # Replace with function body.
 
 func _physics_process(delta):
 	_velocity.y += gravity * delta;
-	_velocity = move_and_slide(_velocity)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- src/characters/Player.gd
+++ src/characters/Player.gd
@@ -1,11 +1,27 @@
 extends Actor
 
-
-# Declare member variables here. Examples:
-var a = 2
-var b = "text"
-
+func _physics_process(delta):
+	print(_velocity)
+	calculate_linear_velocity()
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("move_up") and is_on_floor() else 0.0
+	)
+
+func calculate_linear_velocity():
+	var direction := get_direction()
+	_velocity.y += gravity * get_physics_process_delta_time()
+	_velocity.x = speed.x * direction.x
+
+	if (direction.y):
+		_velocity.y = speed.y * direction.y
+	
