cc776ee4bb000f908b183e48f42b22f116bfec90
PhePheMath <PhelyppeMath@outlook.com>
2021-06-20 01:10:23 -0300

    hierarquia actor>ghost

--- src/characters/Actors.gd
+++ src/characters/Actors.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 class_name Actor
 
-export var gravity:float = 500;
+export var gravity:float = 30;
 export var speed:Vector2 = Vector2(400, 500);
 
 var FLOOR_NORMAL:Vector2 = Vector2.UP
@@ -9,8 +9,8 @@ var _velocity:Vector2 = Vector2.ZERO;
 #func _ready():
 #	pass # Replace with function body.
 
-func _physics_process(delta):
-	_velocity.y += gravity * delta;
+#func _physics_process(delta):
+#	_velocity.y += gravity * delta;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
