0be84b8db3239886a61c4ec2d2064886a5a6adf6
PhePheMath <PhelyppeMath@outlook.com>
2021-06-20 01:05:15 -0300

    adiconando gravidade/simplificando código

--- src/characters/Ghost/Ghost.gd
+++ src/characters/Ghost/Ghost.gd
@@ -1,21 +1,79 @@
-extends KinematicBody2D
+extends Actor
+class_name Ghost
 
-export var gravity = 20
-export var velocity = Vector2(500, 1000)
-export var friction = 15
-export var aceleration = 20
+export var velocity = Vector2(600, 600)
+var friction = 100
+var _wind = 0
+
 var _movimentation = Vector2(0,0)
+var player_gravity = true
+onready var animatedSprite = $AnimatedSprite
+
+
+
+func _on_wind_body_entered(body):
+	_wind = 1
+
+func _on_wind_body_exited(body):
+	_wind = 0
 
 
 func _physics_process(delta):
-	var direction = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-	Input.get_action_strength("move_right") - Input.get_action_strength("move_left"))
-	
-	if direction == Vector2(0,0):
-		_movimentation = _movimentation.move_toward(Vector2(0,0), friction)
-	else:
-		_movimentation = _movimentation.move_toward(direction * velocity, 
-		aceleration)
-	
-	move_and_slide(_movimentation)
 
+	_movimentation = get_movimentation()
+
+	setAnimations()
+
+	if not _wind:
+		_movimentation = move_and_slide(_movimentation, Vector2.UP)
+	else:
+		_wind += calculate_wind()
+		_movimentation.x -= _wind
+		_movimentation.y = 10
+		_movimentation = move_and_slide(_movimentation, Vector2.UP)
+
+
+func setAnimations():
+	if _movimentation.x > 0.0:
+		animatedSprite.play("right")
+
+	if _movimentation.x < 0.0:
+		animatedSprite.play("left")
+
+	if _movimentation.x == 0.0:
+		animatedSprite.play("default")
+
+
+func calculate_linear_velocity():
+	var direction:Vector2 = get_direction()
+	var new:Vector2 = _velocity
+	new.y += gravity * get_physics_process_delta_time()
+	if is_on_floor() or direction.x:
+		new.x = speed.x * direction.x
+
+	if (direction.y):
+		new.y = speed.y * -1
+
+	return new
+
+
+func get_movimentation():
+	var direction = get_direction()
+	var movimentation: Vector2 = _movimentation
+	movimentation.y = 0 if is_on_floor() else gravity
+
+	movimentation.x = speed.x * direction.x if direction.x else 0
+
+	movimentation.y = speed.y * direction.y if direction.y else movimentation.y
+	return movimentation
+
+
+func get_direction():
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+
+
+func calculate_wind():
+	return 30 if _wind < 700 else 700
